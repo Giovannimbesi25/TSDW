4a7d144620e65bc7dc2078b77a84c17fbb0560b1
Giovanni Imbesi <giovanni@pop-os-giovanni.station>
2023-12-10 11:38:22 +0100

    reduce code to only one php file

--- PHP/provePassate/books/index.php
+++ PHP/provePassate/books/index.php
@@ -2,35 +2,42 @@
     <head>
         <title>Libreria</title>
 </head>
-
+<body>
 <?php   
 
-    require_once("database.php");
+    $conn = new mysqli("localhost", "root", "giovanni", "php_db");
 
     if($_SERVER['REQUEST_METHOD'] == "POST"){
         $action = $_POST['action'];
         switch($action){
 
-            case 'list' ; {
-                $books = getAll();
-                echo "<ul>";
-                foreach($books as $book){
-                    ?>
-                    <li>
-                    <h4>ISBN: <a href="index.php?isbn=<?= $book['isbn'] ?>"><?= $book['isbn'] ?></a></h4>
-                    <h4>Title: <?= $book['title'] ?></h4>
-                    <h4>Author: <?= $book['author'] ?></h4>
-                    <h4>Ranking: <?= $book['ranking'] ?></h4>
-                    <h4>Year: <?= $book['year'] ?></h4>
-                    <h4>Price: <?= $book['price'] ?></h4>
-                    <br><br>
-                    </li>
-                    <?php
+            case 'list' : {
+                echo "<h2>Lista Libri</h2>";
+
+                $sql = "SELECT * from books";
+                $result = $conn->query($sql);
+                $books = $result->fetch_all(MYSQLI_ASSOC);
+                if($books != null){
+                    echo "<ul>";
+                    foreach($books as $book){
+                        ?>
+                        <li>
+                        <h4>ISBN: <a href="index.php?isbn=<?= $book['isbn'] ?>"><?= $book['isbn'] ?></a></h4>
+                        <h4>Title: <?= $book['title'] ?></h4>
+                        <h4>Author: <?= $book['author'] ?></h4>
+                        <h4>Ranking: <?= $book['ranking'] ?></h4>
+                        <h4>Year: <?= $book['year'] ?></h4>
+                        <h4>Price: <?= $book['price'] ?></h4>
+                        <br><br>
+                        </li>
+                        <?php
+                    }
+                    echo "</ul";
+                }else{
+                    echo "Nessun libro nella libreria";
                 }
-                echo "</ul";
-
                 ?>  
-
+                <br><br>
                 <h2>Aggiungi un libro</h2>
 
                 <form method="post">
@@ -55,16 +62,15 @@
                 $year = $_POST['year'];
                 $price = $_POST['price'];
 
-                if(addBook($isbn, $title, $author, $ranking, $year, $price )){
-                    echo "<h2>Libro inserito con successo</h2>";
-
-                    
-                    header("Location: index.php?");
-                
+                $sql = "INSERT INTO books (isbn, title, author, ranking, year, price) VALUES(?,?,?,?,?,?)";
+                $stm = $conn->prepare($sql);
+                $stm->bind_param("ssssss", $isbn, $title, $author, $ranking, $year, $price);
+                if($stm->execute()){
+                    header("Location: index.php");
                 }else{
                     echo "Errore aggiunta libro";
-                }
-                
+                }         
+                $stm->close();       
             }break;
 
             case 'update' ; {
@@ -75,7 +81,10 @@
                 $year = $_POST['year'];
                 $price = $_POST['price'];
 
-                if(updateBook($isbn,$title, $author, $ranking, $year, $price )){
+                $sql = "UPDATE books SET title= ?, author = ?, ranking=?, year=?, price=?";
+                $stm = $conn->prepare($sql);
+                $stm->bind_param("sssss", $title, $author, $ranking, $year, $price);
+                if($stm->execute()){
                     echo "<h2>Libro modificato con successo</h2>";
                     ?>
                     <?php
@@ -90,8 +99,10 @@
 
             case 'delete' ; {
                 $isbn = $_POST['isbn'];
-                
-                if(deleteBook()){
+                $sql = "DELETE from books WHERE isbn = ?";
+                $stm = $conn->prepare($sql);
+                $stm->bind_param("s", $isbn);
+                if($stm->execute()){
                     echo "<h2>Libro eliminato con successo</h2>";
                     ?>
                     <a href="index.php">Ritorna alla pagina principale</a>
@@ -108,25 +119,32 @@
 
         if($_GET['isbn'] != null){
             $isbn = $_GET['isbn'];
+            $sql = "SELECT * from books WHERE isbn=?";
+            $stm = $conn->prepare($sql);
+            $stm->bind_param("s", $isbn);
+            if($stm->execute()){
+                $result = $stm->get_result();
+                $book = $result->fetch_assoc();
+                ?>
+                <form method="post">
+                <h4>ISBN: <input value="<?= $isbn ?>" name="isbn" required type="text"?></h4>
+                <h4>Title: <input value="<?= $book['title']?>" name="title" required type="text" /></h4>
+                <h4>Author: <input value="<?= $book['author']?>" name="author" required type="text" /></h4>
+                <h4>Ranking: <input value="<?= $book['ranking']?>" name="ranking" required type="text" /></h4>
+                <h4>Year: <input value="<?= $book['year']?>" name="year" required type="text" /></h4>
+                <h4>Price: <input value="<?= $book['price']?>" name="price" required type="text" /></h4>
+                <input name="action" value="update" type="submit" />
+                <input name="action" value="delete" type="submit" />
+                </form>
 
-            $book = getBook($isbn);
-            ?>
-            <form method="post">
-            <h4>ISBN: <input value="<?= $isbn ?>" name="isbn" required type="text"?></h4>
-            <h4>Title: <input value="<?= $book['title']?>" name="title" required type="text" /></h4>
-            <h4>Author: <input value="<?= $book['author']?>" name="author" required type="text" /></h4>
-            <h4>Ranking: <input value="<?= $book['ranking']?>" name="ranking" required type="text" /></h4>
-            <h4>Year: <input value="<?= $book['year']?>" name="year" required type="text" /></h4>
-            <h4>Price: <input value="<?= $book['price']?>" name="price" required type="text" /></h4>
-            <input name="action" value="update" type="submit" />
-            <input name="action" value="update" type="submit" />
-            </form>
+                <br><br>
 
-            <br><br>
+                <a href="index.php">Ritorna alla pagina principale</a>
 
-            <a href="index.php">Ritorna alla pagina principale</a>
-
-            <?php
+                <?php
+            }else{
+                echo "Error retrieving book";
+            }
             
 
         }else{
@@ -142,7 +160,13 @@
             </form>
             <?php
         }
+
+        $conn->close();
+        
     }
+?>
+</body>
+</html>
     
 
 
--- PHP/provePassate/filmWishList/index.php
+++ PHP/provePassate/filmWishList/index.php
@@ -1,29 +1,144 @@
 <html>
-  <?php
-    require_once('database.php');
-
-    createTables();
-  ?> 
-  <head>
-    <title>Film Wish List</title>
-</head>
+<head><title>FakeFlix</title></head>
 <body>
-  <center>
-    <h1>Benvenuto nella Film Wish List</h1><br><br>
+<?php
+    //localhost, username, password, dbname
+    $conn = new mysqli("localhost", "root", "giovanni", "php_db") or die("Connection error");
+
+    if($_SERVER['REQUEST_METHOD']=='GET'){
+        echo "<h1>Benvenuto su fakeflix</h1><br>";
+        $sql = "SELECT * from flist ORDER BY RAND() LIMIT 1";
+        $result = $conn->query($sql);
+        if($result->num_rows > 0){
+            $film = $result->fetch_assoc();
+            echo "<h1>Film consigliato " . $film['titolo'] . "</h1>";
+        }else{
+            echo "<h1>Lista film vuota</h1>";
+        }
+
+        echo "<br><h2>Inserisci il titolo e/o il regista del film che vuoi cercare</h2>";
+
+        ?>
+
+        <form method="post">
+            Titolo: <input name="titolo" type="text"/>
+            Regista: <input name="regista" type="text"/>
+            <input type="submit" value="search" name="action"/>
+        </form>
+        <?php
+
+
+        echo "<br><h2>Premi per vedere la tua wlist</h2>";
+        ?>
+        <form method="post">
+        <input type="submit" name="action" value="wlist"/>
+        </form>
+        <?php
+
+    }
+
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+        $action = $_POST['action'];
+
+        switch($action){
+            case 'search': {
+                $titolo = $_POST['titolo'];
+                $regista = $_POST['regista'];
+                if($titolo == "" AND $regista == ""){
+                    echo "<h3>Uno dei due campi deve essere riempito</h3>";
+                    echo "<a href='index.php'><h3>Ritorna alla home</h3></a>";
+                }else{
+                    $sql = "SELECT * FROM flist WHERE titolo=? OR regista=?";
+                    $stm = $conn->prepare($sql);
+                    $stm->bind_param("ss", $titolo, $regista);
+                    if($stm->execute()){
+                        $result = $stm->get_result();
+                        if($result->num_rows > 0){
+                            $films = $result->fetch_all(MYSQLI_ASSOC);
+                            echo "<ul>";
+                            foreach($films as $film){
+                                echo "<li>";
+                                echo "<h3>Titolo: " . $film['titolo'] ."</h3>";
+                                echo "<h3>Regista: " . $film['regista']. "</h3>";
+                            }
+                            echo "</ul>";
+                        }else{
+                            if($titolo != "" AND $regista != ""){
+                                echo "<h2>Vuoi inserire questo film nella wlist?</h2>";
+                                ?>
+                                <form method="post">
+                                <input name="titolo" type="text" hidden value="<?= $titolo ?>" />
+                                <input name="regista" type="text" hidden value="<?= $regista ?>" />
+                                <input type="submit" value="si" name="action"/>
+                                <input type="submit" value="no" name="action"/>
+                                </form>
+                                <?php
+                            }else{
+                                echo "<h2>Nessun film trovato con questi valori</h2>";
+                                echo "<a href='index.php'><h3>Ritorna alla home</h3></a>";
+                            }   
+                        }
+                    }
+                }
+
+            }break;
+            
+            case 'no' : {
+                header("Location: index.php");
+            }break;
+            case 'si' : {
+                $titolo = $_POST['titolo'];
+                $regista = $_POST['regista'];
+                $sql = "INSERT INTO wlist (titolo, regista) VALUES(?,?)";
+                $stm = $conn->prepare($sql);
+                $stm->bind_param("ss", $titolo, $regista);
+
+                if($stm->execute()){
+                    echo "<h2>Film aggiunto con successo</h2>";
+                    echo "<a href='index.php'><h3>Ritorna alla home</h3></a>";
+                }
+            }break;
+
+            case 'wlist' : {
+                $sql = "SELECT * from wlist";
+                $result = $conn->query($sql);
+                if($result->num_rows > 0){
+                    $films = $result->fetch_all(MYSQLI_ASSOC);
+                    echo "<ul>";
+                    foreach($films as $film){
+                        echo "<li>";
+                        echo "<h3>Titolo: " . $film['titolo'] ."</h3>";
+                        echo "<h3>Regista: " . $film['regista']. "</h3>";
+                    }
+                    echo "</ul>";
+
+                    ?>
+                    <form method="post">
+                    <input type="submit" name="action" value="svuota">
+                    </form>
+                    <?php
+                }else{
+                    echo "<h1>Wlist vuota</h1>";
+                }
+
+                echo "<a href='index.php'><h4>Torna alla home</h4></a>";
+            }break;
+
+            case 'svuota' : {
+                $sql = "TRUNCATE wlist";
+                $conn->query($sql);
+                echo "<h2>WLIST pulita</h2>";
+                echo "<a href='index.php'><h4>Torna alla home</h4></a>";
+            }
+            
+            
+        }
+    }
 
     
-    
 
-    <h1>Film Consigliato: <?= getRandomFilmTitle()?></h1><br><br>
-    <h2>
-      <li>
-        <a href="search.php">Cerca un film</a>
-    </l1>
-    </h2>
-    <h2>
-      <li>
-        <a href="show_wlist.php">Mostra la mia wlist</a>
-    </l1>
-    </h2>
-</center>
+
+    $conn->close();
+?>
 </body>
+</html>
